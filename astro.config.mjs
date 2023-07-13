092935942562f0f797cf4ccb47a7f9c5ecde78ea
Matthew Tao <matthew.yingtao@gmail.com>
2023-07-13 18:12:41 +1200

    assadsdasdasdaasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
