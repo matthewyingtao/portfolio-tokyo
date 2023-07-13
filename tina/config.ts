b222620aa5c878cc150305aba0de81fdb4ff8489
Matthew Tao <matthew.yingtao@gmail.com>
2023-07-13 17:23:45 +1200

    sdfasdfaasdffsdadfs

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
 	branch,
