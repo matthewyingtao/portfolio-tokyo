104a090049e4acc7243db17ce0b17c83349a5f8f
Matthew Tao <matthew.yingtao@gmail.com>
2023-07-13 17:45:02 +1200

    sddsadsa

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,11 @@
 import rss from "@astrojs/rss";
-import slugify from "slugify";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function get(context) {
-	const posts = await Astro.glob("../posts/*.{md,mdx}");
-
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${slugify(post.file.replace(/\.md(x)?/, ""))}/`,
-		})),
+		items: import.meta.glob("../posts/*.{md,mdx}"),
 	});
 }
