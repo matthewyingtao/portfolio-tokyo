dd3407e36669a39c16354f56dc52018376d2472b
Matthew Tao <matthew.yingtao@gmail.com>
2025-03-31 13:49:18 +1300

    add prettier-plugin-tailwindcss package

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-astro")],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.astro",
