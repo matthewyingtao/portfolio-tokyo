89cdf82aa591ea10cf9a47cac60a33bb75f4e845
Matthew Tao <matthew.yingtao@gmail.com>
2025-03-31 14:39:29 +1300

    add tailwind vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 import themeRosePineDawn from "/src/rose-pine-dawn-color-theme.json";
@@ -7,6 +8,9 @@ import themeRosePineDawn from "/src/rose-pine-dawn-color-theme.json";
 export default defineConfig({
   site: "https://www.matthewtao.com",
   integrations: [sitemap(), robotsTxt()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
     shikiConfig: {
       theme: themeRosePineDawn,
